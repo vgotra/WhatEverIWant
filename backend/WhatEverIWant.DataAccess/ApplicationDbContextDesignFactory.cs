192dfaf1f0bd84e9b689d61125e5e25c95652626
Volodymyr Gotra <vgotra@gmail.com>
2024-10-09 23:35:39 +0300

    Updated DAL, updated services, etc.

---
+++ backend/WhatEverIWant.DataAccess/ApplicationDbContextDesignFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace WhatEverIWant.DataAccess;
+
+public class ApplicationDbContextDesignFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlite("Data Source=:memory:");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
--- backend/WhatEverIWant/Program.cs
+++ backend/WhatEverIWant/Program.cs
@@ -29,7 +29,6 @@ if (app.Environment.IsDevelopment())
     using var scope = app.Services.CreateScope();
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     context.Database.EnsureCreated();
-    context.Database.Migrate();
     */
 }
 
